17bc567d444a78b85409f27a0893323cf1a34916
Yunus Mulla <mullayunus449@gmail.com>
2025-10-22 18:40:40 +0400

    Work Exp section UI fixed

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin'
-import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
 
